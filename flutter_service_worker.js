927b598a238d4f977af22c13b5654027d32a5577
mhussnainshabbir <69431278+mhussnainshabbir@users.noreply.github.com>
2022-11-29 01:11:43 +0500

    Add files via upload
    fixes.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -84,9 +84,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "0a08a9c2e8d124c9f898e8f4a49740dd",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "aad4f5c926f3e544e5d20a969bde6c04",
-"/": "aad4f5c926f3e544e5d20a969bde6c04",
-"main.dart.js": "d6f12e10e33424d2cd80f57a0c6b96e5",
+"index.html": "b9df842c18731a7972d747d852aaca71",
+"/": "b9df842c18731a7972d747d852aaca71",
+"main.dart.js": "98a18f07b2278bb7e072e533c9911058",
 "manifest.json": "d33c0df3889084245e4a2b217e87921c",
 "version.json": "b85b6ffa562fd034f2ee9aa5a0e2b9dd"
 };
